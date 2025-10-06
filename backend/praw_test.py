63af784fb99f436ee5b63c096f732c02575074a9
Ethan <eencollins@gmail.com>
2025-10-05 21:10:48 -0500

    praw test and csv test. Major wip.

---
+++ backend/praw_test.py
@@ -0,0 +1,43 @@
+#important note: This file will most likely get delete in the near future
+# this is here now only for testing and progression purposes.
+#The "chopped_chin" csv file is an test.
+
+import pandas as pd
+import numpy as np
+import praw
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+
+user_agent = "Praw Test 1.0 by /u/Odd_Interaction5417"
+reddit = praw.Reddit(
+    client_id="zY-ILX9sWXY30v13HTu5gA",
+    client_secret="dXYmApEGGlawyCWIySd-ZR01TWNhZw",
+    user_agent = user_agent
+)
+
+headlines = set()
+for submission in reddit.subreddit('cscareerquestions').new(limit=None):
+    headlines.add(submission.title)
+
+df = pd.DataFrame(headlines)
+
+
+nltk.download('vader_lexicon')
+sia = SIA()
+result = []
+
+for line in headlines:
+    pol_score = sia.polarity_scores(line)
+    pol_score['headlines'] = line
+    result.append(pol_score)
+
+df = pd.DataFrame.from_records(result)
+
+
+df['label'] = 0
+df.loc[df['compound'] > 0.2, 'label'] = 1
+df.loc[df['compound'] < -0.2, 'label'] = -1
+df.head()
+
+df2 = df[['headlines', 'label']]
+df2.to_csv('chopped_chin.csv', encoding='utf-8', index=False)
